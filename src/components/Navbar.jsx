411bf5479d0b7fb521d8edd896bbfc0595d03bac
Abu Hanjala <abuhanjala656@gmail.com>
2026-05-05 18:11:09 +0530

    add search features and also use  Context for data centralize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { CiSearch } from 'react-icons/ci'
 import { IoFastFood } from 'react-icons/io5'
 import { RiShoppingBag4Line } from 'react-icons/ri'
+import { dataContext } from '../context/UserContext'
+import food_items from '../food'
 
 const Navbar = () => {
-  return (
+const {input , setInput , cate , setCate} = useContext(dataContext)
+  
+useEffect(()=>{
+  const newList = food_items.filter((ftm)=>ftm.food_name.includes(input) ||ftm.food_name.toLowerCase().includes(input) );
+  setCate(newList)
+},[input])
+
+return (
     <div className='flex justify-between items-center px-8 py-6'>
       <div className='text-white bg-[#FF5200] rounded-lg p-3.5 shadow-xl'>
         <IoFastFood className='size-6' />
       </div>
 
-      <form className='flex h-14 w-[50%] bg-amber-50 rounded-full items-center gap-4 px-5 shadow-sm'>
+      <form
+      onSubmit={(e)=>e.preventDefault()}
+      className='flex h-14 w-[50%] bg-amber-50 rounded-full items-center gap-4 px-5 shadow-sm'>
         <CiSearch className='size-6 text-gray-400' />
         <input
+        onChange={(e)=> setInput(e.target.value)}
+         value={input}
           className='w-full bg-transparent outline-none text-gray-600 placeholder-gray-400'
           type="text"
           placeholder='search items...'
@@ -27,4 +40,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Card from "../components/Card";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
 import food_items from "../food";
+import { dataContext } from "../context/UserContext";
 
 const Home = () => {
-  const [cate , setCate] = useState(food_items);
+  const { cate, setCate, input } = useContext(dataContext);
 
-  const filterFood = (category) =>{
-       if(category === "All"){
-        setCate(food_items);
-       }
-       else{
-        const newList =  food_items.filter((f)=>f.food_category === category.toLowerCase() );
-        setCate(newList);
-       }
+  const filterFood = (category) => {
+    if (category === "All") {
+      setCate(food_items);
+    }
+    else {
+      const newList = food_items.filter((f) => f.food_category === category.toLowerCase());
+      setCate(newList);
+    }
   }
 
 
@@ -22,17 +23,15 @@ const Home = () => {
     <div className="bg-amber-100 min-h-screen">
       <Navbar />
 
-      {/* Categories */}
-      <div className="flex flex-wrap justify-center items-center gap-3 sm:gap-4 mt-20 px-3">
+      {!input ? <div className="flex flex-wrap justify-center items-center gap-3 sm:gap-4 mt-20 px-3">
         {Categories.map((item) => {
           return (
             <div
-             
               key={item.id}
               className="border rounded-full p-1 sm:p-2 text-black/40 hover:text-[#e1652b]"
             >
               <div
-              onClick={()=>filterFood(item.name)}
+                onClick={() => filterFood(item.name)}
                 className="bg-transparent rounded-full flex flex-col justify-center items-center 
               w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 
               text-orange-500 cursor-pointer 
@@ -45,7 +44,7 @@ const Home = () => {
             </div>
           );
         })}
-      </div>
+      </div> : null}
 
       {/* Food Cards */}
       <div className="mt-8 px-3 sm:px-6 md:px-10 lg:px-16  pb-6 ">
